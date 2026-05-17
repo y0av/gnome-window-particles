20224843d63363ec22a6476d16ac4e6d0b10e45c
y0av <mail.yoav@gmail.com>
2026-05-17 13:44:26 +0300

    fixed extension for wayland new standards

--- extension.js
+++ extension.js
@@ -1,43 +1,66 @@
-const { Extension } = imports.misc.extensionUtils;
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 
-const Me = imports.misc.extensionUtils.getCurrentExtension();
-const ParticleEngine = Me.imports['particle-engine'].ParticleEngine;
-const EffectType = Me.imports['effects'].EffectType;
+import { EffectType } from './effects.js';
+import { ParticleEngine } from './particle-engine.js';
 
-var WindowParticlesExtension = class extends Extension {
+export default class WindowParticlesExtension extends Extension {
   enable() {
     this.particleEngine = new ParticleEngine();
     this.currentEffect = EffectType.SPARKLES; // Default effect
+    this._windowSignals = new Map();
 
-    this._windowClosedId = global.display.connect(
-      'window-closed',
-      this._onWindowClosed.bind(this)
+    this._windowCreatedId = global.display.connect(
+      'window-created',
+      this._onWindowCreated.bind(this)
     );
+
+    for (const windowActor of global.get_window_actors())
+      this._trackWindow(windowActor.meta_window);
     
     log('[Window Particles] Extension enabled with effect: ' + this.currentEffect);
   }
 
   disable() {
-    if (this._windowClosedId) {
-      global.display.disconnect(this._windowClosedId);
+    if (this._windowCreatedId) {
+      global.display.disconnect(this._windowCreatedId);
+      this._windowCreatedId = null;
     }
+
+    for (const [window, signalId] of this._windowSignals) {
+      if (window && signalId)
+        window.disconnect(signalId);
+    }
+    this._windowSignals.clear();
     
     if (this.particleEngine) {
       this.particleEngine.cleanup();
+      this.particleEngine = null;
     }
     
     log('[Window Particles] Extension disabled');
   }
 
-  _onWindowClosed(display, window) {
-    const [x, y, width, height] = window.get_frame_rect();
-    const centerX = x + width / 2;
-    const centerY = y + height / 2;
+  _onWindowCreated(display, window) {
+    this._trackWindow(window);
+  }
+
+  _trackWindow(window) {
+    if (!window || this._windowSignals.has(window))
+      return;
+
+    const signalId = window.connect('unmanaged', () => {
+      this._windowSignals.delete(window);
+      this._onWindowClosed(window);
+    });
+
+    this._windowSignals.set(window, signalId);
+  }
+
+  _onWindowClosed(window) {
+    const frameRect = window.get_frame_rect();
+    const centerX = frameRect.x + frameRect.width / 2;
+    const centerY = frameRect.y + frameRect.height / 2;
 
     this.particleEngine.spawnEffect(centerX, centerY, this.currentEffect);
   }
-};
-
-function init() {
-  return new WindowParticlesExtension();
 }
